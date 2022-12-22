4c261495e9510e7094aa7bfce30c927fcf143bc3
alixonsanchez <121204924+alixonsanchez@users.noreply.github.com>
2022-12-21 20:44:42 -0500

    Update codBash05.sh

--- codBash05.sh
+++ codBash05.sh
@@ -14,3 +14,39 @@ read -p "ingrese su numero de telefono: " telefono
 
 if [[ $edad =~ $regexedad ]]; then
 	echo "edad valida"
+else
+	echo "edad invalida"
+fi
+
+if [[ $nombre =~ $regexnombre ]]; then
+	echo "nombre valido"
+else
+	echo "nombre invalido"
+fi
+
+if [[ $apellido =~ $regexapellido ]]; then
+	echo "apellido valido"
+else
+	echo "apellido invalido"
+fi
+
+if [[ $direccion =~ $regexdireccion ]]; then
+	echo "Dirección valida"
+else
+	echo "Dirección invalida"
+fi
+
+
+if [[ $telefono =~ $regextelefono ]]; then
+	echo "telefono valido"
+else
+	echo "telefono invalido"
+fi
+
+echo "los datos"
+echo $nombre
+echo $apellido
+echo $edad
+echo $direccion
+echo $telefono
+
